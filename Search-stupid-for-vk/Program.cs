289fd24b9d0aa934bcc452107d95ab675ffe8a2b
DiZiNnEs <dizinnes228@mail.ru>
2020-07-25 15:12:14 +0600

    Start project on C#

---
+++ Search-stupid-for-vk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Search_stupid_for_vk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Fan project");
+        }
+    }
+}
