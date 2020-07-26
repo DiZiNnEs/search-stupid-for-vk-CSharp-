b392e9f95344aca10151733f4fa18f7b75a567ea
DiZiNnEs <dizinnes228@mail.ru>
2020-07-26 11:07:26 +0600

    Added property, generic and constructor

--- Search-stupid-for-vk/TheFirstWayToFindDumb.cs
+++ Search-stupid-for-vk/TheFirstWayToFindDumb.cs
@@ -1,7 +1,21 @@
+using System;
+using System.Net.Http;
+using System.Threading.Channels;
+
 namespace Search_stupid_for_vk
 {
-    public class TheFirstWayToFindDumb
+    public class TheFirstWayToFindDumb<T>
     {
-        
+        private string url;
+
+        public TheFirstWayToFindDumb(string url)
+        {
+            this.url = url;
+        }
+
+        public string Test()
+        {
+            return url;
+        }
     }
 }
