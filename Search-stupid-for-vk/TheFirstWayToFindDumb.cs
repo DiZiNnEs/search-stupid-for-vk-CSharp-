2b115326628c2273bb50465b83ee2b0d54e0d0c1
DiZiNnEs <dizinnes228@mail.ru>
2020-07-26 11:36:28 +0600

    Last update) I'm lazy)

--- Search-stupid-for-vk/TheFirstWayToFindDumb.cs
+++ Search-stupid-for-vk/TheFirstWayToFindDumb.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Net.Http;
 using System.Threading.Channels;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Dom;
 
 namespace Search_stupid_for_vk
 {
@@ -17,6 +20,21 @@ namespace Search_stupid_for_vk
         {
             return $"I got a link: {url}\n" +
                    $"Searching for an asshole";
+        }
+
+        public async Task GetHtml()
+        {
+            HttpClient httpClient = new HttpClient();
+            string url = await httpClient.GetStringAsync(this.url);
+            IConfiguration config = Configuration.Default;
+            IBrowsingContext context = BrowsingContext.New(config);
+            IDocument document = await context.OpenAsync(request => request.Content(url));
+
+            var got = document.Links;
+            foreach (var VARIABLE in got)
+            {
+                Console.WriteLine(VARIABLE.TextContent);
+            }
         }
     }
 }
