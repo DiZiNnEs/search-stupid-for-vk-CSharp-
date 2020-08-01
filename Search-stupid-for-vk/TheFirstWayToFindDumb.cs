efd6ad5105b9a2c26b3c74d4891be1745c54600a
DiZiNnEs <dizinnes228@mail.ru>
2020-08-02 01:10:00 +0600

    Add method for read files and find black word

--- Search-stupid-for-vk/TheFirstWayToFindDumb.cs
+++ Search-stupid-for-vk/TheFirstWayToFindDumb.cs
@@ -31,8 +31,8 @@ namespace Search_stupid_for_vk
             string content = await httpClient.GetStringAsync(url);
             try
             {
-                // StreamWriter textFile = new StreamWriter("parsingResult.txt");
-                // textFile.Write(content);
+                StreamWriter textFile = new StreamWriter("parsingResult.txt");
+                textFile.Write(content);
             }
             catch (DirectoryNotFoundException ex)
             {
@@ -65,61 +65,21 @@ namespace Search_stupid_for_vk
                 throw new Exception();
             }
         }
-        // Сделать метод для прочтение этого файла
 
         public void ReadToTextFile()
         {
             string[] wordForFindDumb = {"Kuat", "Electron", "Sex18"};
-
-            // string[] lines = File.ReadAllLines("parsingResult.txt");
-            // foreach (string line in lines)
-            //     Console.WriteLine(line);
-            // Console.WriteLine("FIND THE WORD");
-            // Console.Write("Keyword: ");
-            // var keyword = Console.ReadLine() ?? "";
-            // using (var sr = new StreamReader("parsingResult.txt")) {
-            //     while (!sr.EndOfStream) {
-            //         var line = sr.ReadLine();
-            //         if (String.IsNullOrEmpty(line)) continue;
-            //         if (line.IndexOf(keyword, StringComparison.CurrentCultureIgnoreCase) >= 0) {
-            //             Console.WriteLine(line);
-            //         }
-            //     }
-            // }
-            //We read all the lines from the file
-//             IEnumerable<string> lines = File.ReadAllLines("parsingResult.txt");
-//
-// //We read the input from the user
-//             Console.Write("Enter the word to search: ");
-//             string input = Console.ReadLine().Trim();
-//             // var input = wordForFindDumb;
-//
-// //We identify the matches. If the input is empty, then we return no matches at all
-//             IEnumerable<string> matches = !String.IsNullOrEmpty(input)
-//                 ? lines.Where(line => line.IndexOf(input, StringComparison.OrdinalIgnoreCase) >= 0)
-//                 : Enumerable.Empty<string>();
-//
-// //If there are matches, we output them. If there are not, we show an informative message
-//             Console.WriteLine(matches.Any()
-//                 ? String.Format("Matches:\n> {0}", String.Join("\n> ", matches))
-//                 : "There were no matches");
-
             foreach (var input in wordForFindDumb)
             {
                 Console.WriteLine($"Found the word: {input}");
-                //We read all the lines from the file
                 IEnumerable<string> lines = File.ReadAllLines("parsingResult.txt");
 
-//We read the input from the user
                 Console.Write("Enter the word to search: ");
-                // string input = Console.ReadLine().Trim();
 
-//We identify the matches. If the input is empty, then we return no matches at all
                 IEnumerable<string> matches = !String.IsNullOrEmpty(input)
                     ? lines.Where(line => line.IndexOf(input, StringComparison.OrdinalIgnoreCase) >= 0)
                     : Enumerable.Empty<string>();
 
-//If there are matches, we output them. If there are not, we show an informative message
                 Console.WriteLine(matches.Any()
                     ? String.Format("Matches:\n> {0}", String.Join("\n> ", matches))
                     : "There were no matches");
