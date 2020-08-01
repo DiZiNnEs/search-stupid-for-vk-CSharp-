428fa0dd3bb038dc60d027f3e35844dfd8133ccf
DiZiNnEs <dizinnes228@mail.ru>
2020-08-01 18:29:15 +0600

    Made the test method

--- Search-stupid-for-vk/TheFirstWayToFindDumb.cs
+++ Search-stupid-for-vk/TheFirstWayToFindDumb.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -70,10 +71,62 @@ namespace Search_stupid_for_vk
         {
             string[] wordForFindDumb = {"Kuat", "Electron"};
 
-            string[] lines = File.ReadAllLines("parsingResult.txt");
-            foreach (string line in lines)
-                Console.WriteLine(line);
-            Console.WriteLine("FIND THE WORD");
+            // string[] lines = File.ReadAllLines("parsingResult.txt");
+            // foreach (string line in lines)
+            //     Console.WriteLine(line);
+            // Console.WriteLine("FIND THE WORD");
+            Console.Write("Keyword: ");
+            var keyword = Console.ReadLine() ?? "";
+            using (var sr = new StreamReader("parsingResult.txt")) {
+                while (!sr.EndOfStream) {
+                    var line = sr.ReadLine();
+                    if (String.IsNullOrEmpty(line)) continue;
+                    if (line.IndexOf(keyword, StringComparison.CurrentCultureIgnoreCase) >= 0) {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+
+            Console.ReadLine();
+            //We read all the lines from the file
+//             IEnumerable<string> lines = File.ReadAllLines("parsingResult.txt");
+//
+// //We read the input from the user
+//             Console.Write("Enter the word to search: ");
+//             string input = Console.ReadLine().Trim();
+//             // var input = wordForFindDumb;
+//
+// //We identify the matches. If the input is empty, then we return no matches at all
+//             IEnumerable<string> matches = !String.IsNullOrEmpty(input)
+//                 ? lines.Where(line => line.IndexOf(input, StringComparison.OrdinalIgnoreCase) >= 0)
+//                 : Enumerable.Empty<string>();
+//
+// //If there are matches, we output them. If there are not, we show an informative message
+//             Console.WriteLine(matches.Any()
+//                 ? String.Format("Matches:\n> {0}", String.Join("\n> ", matches))
+//                 : "There were no matches");
+
+
+            Console.ReadLine();
+            foreach (var input  in wordForFindDumb)
+            {
+                //We read all the lines from the file
+                IEnumerable<string> lines = File.ReadAllLines("parsingResult.txt");
+
+//We read the input from the user
+                Console.Write("Enter the word to search: ");
+                // string input = Console.ReadLine().Trim();
+
+//We identify the matches. If the input is empty, then we return no matches at all
+                IEnumerable<string> matches = !String.IsNullOrEmpty(input)
+                    ? lines.Where(line => line.IndexOf(input, StringComparison.OrdinalIgnoreCase) >= 0)
+                    : Enumerable.Empty<string>();
+
+//If there are matches, we output them. If there are not, we show an informative message
+                Console.WriteLine(matches.Any()
+                    ? String.Format("Matches:\n> {0}", String.Join("\n> ", matches))
+                    : "There were no matches");
+            }
         }
     }
 }
