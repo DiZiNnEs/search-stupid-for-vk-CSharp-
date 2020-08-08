c84dff7f7d840d530a399eb65fbfbcc06de785ea
DiZiNnEs <dizinnes228@gmail.com>
2020-08-08 18:22:32 +0600

    future: added NUinit and Selenium, update TheSecondWayToFindDumb.cs

--- Search-stupid-for-vk/TheSecondWayToFindDumb.cs
+++ Search-stupid-for-vk/TheSecondWayToFindDumb.cs
@@ -1,3 +1,13 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
 namespace Search_stupid_for_vk
 {
     public class TheSecondWayToFindDumb
